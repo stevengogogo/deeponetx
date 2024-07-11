3bad8c22573b40917ae827469739a03c21eac831
Shao-Ting Chiu <stchiu@email.tamu.edu>
2024-07-11 09:39:11 -0500

    use batch data in train

--- deeponetx/train.py
+++ deeponetx/train.py
@@ -10,10 +10,7 @@ from .data.data import DatasetDeepONet, DataDeepONet
 
 
 def predict(model:AbstractDeepONet, data:DatasetDeepONet):
-    return jax.vmap(
-        jax.vmap(
-            model, in_axes=(None, 0)), 
-        in_axes=(0, None))(data.input_branch, data.input_trunk)
+    return jax.vmap(model, in_axes=(0,0))(data.input_branch, data.input_trunk)
 
 def loss_fn(model:AbstractDeepONet, data:DataDeepONet):
 
@@ -25,12 +22,13 @@ def loss_fn(model:AbstractDeepONet, data:DataDeepONet):
 @eqx.filter_jit
 def update_fn(model:AbstractDeepONet, data:DataDeepONet, optimizer, state):
     loss, grad = eqx.filter_value_and_grad(loss_fn)(model, data)
+    #jax.debug.print("{w}", w=grad.net_branch.layers[0].weight[0])
     updates, new_state = optimizer.update(grad, state, model)
     new_model = eqx.apply_updates(model, updates)
     return new_model, new_state, loss
 
 
-def train(model:AbstractDeepONet, dataloader:DataDeepONet, optimizer, n_iter:int):
+def train(model:AbstractDeepONet, dataloader:DatasetDeepONet, optimizer, n_iter:int):
     data_iter = iter(dataloader)
     state = optimizer.init(
         eqx.filter(model, eqx.is_array)
