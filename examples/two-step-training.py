6516badf89db2700a75eed27eb74d26f89978637
Shao-Ting Chiu <stchiu@email.tamu.edu>
2024-08-20 16:54:19 -0500

    init

---
+++ examples/two-step-training.py
@@ -0,0 +1,13 @@
+#%%
+import jax
+import jax.numpy as jnp
+import jax.random as jr
+import equinox as eqx
+import optax
+import matplotlib.pyplot as plt
+from tqdm.autonotebook import tqdm
+
+
+
+
+# %%
